caa70f2e1ed7db556cf5094025b4c843d3a29e2c
agina <mohammed.awadallah@gmail.com>
2018-08-20 13:43:59 -0800

    last

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC_MHaJ5jVxw1zFx-Gq-BsEalQMTnmPLw8",
+    authDomain: "employeesng.firebaseapp.com",
+    databaseURL: "https://employeesng.firebaseio.com",
+    projectId: "employeesng",
+    storageBucket: "employeesng.appspot.com",
+    messagingSenderId: "911287868"
+  }
 };
