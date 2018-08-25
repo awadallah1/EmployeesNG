7b916c67f41f8d7062bcb3344476b637ce381c22
agina <mohammed.awadallah@gmail.com>
2018-08-25 14:18:43 -0800

    adapt image upload

--- src/app/components/add-employee/add-employee.component.ts
+++ src/app/components/add-employee/add-employee.component.ts
@@ -3,6 +3,11 @@ import { Employee } from "../../interfaces/Employee";
 import { ToastrService } from 'ngx-toastr';
 import { EmployeeService } from '../../services/employee.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask } from 'angularfire2/storage';
+import { finalize } from 'rxjs/operators';
+
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-add-employee',
@@ -11,17 +16,23 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class AddEmployeeComponent implements OnInit {
 
+  ref: AngularFireStorageReference;
+  task: AngularFireUploadTask;
+
+  downloadURL: Observable<string>;
+  imageURL: string
   employee: Employee = {};
   btnTitle: string = 'Submit'
   forEdit: boolean = false;
   picFile: File;
+  myEvent: any;
 
   url: string
 
   id: string = this._route.snapshot.queryParams["id"];
   oldImage: string;
 
-  constructor(private _route: ActivatedRoute, private empService: EmployeeService, private _Router: Router, private toastr: ToastrService) { }
+  constructor(private afStorage: AngularFireStorage, private _route: ActivatedRoute, private empService: EmployeeService, private _Router: Router, private toastr: ToastrService) { }
 
   public ngOnInit() {
 
@@ -29,7 +40,7 @@ export class AddEmployeeComponent implements OnInit {
       this.btnTitle = 'Edit'
       this.forEdit = true;
       this.empService.getEmployee(this.id).subscribe(action => {
-       
+
         let employee = action.payload.val() as Employee;
         if (employee.image) { this.oldImage = employee.image; }
 
@@ -42,9 +53,9 @@ export class AddEmployeeComponent implements OnInit {
         this.employee.city = employee.city;
         this.employee.phone = employee.phone;
         this.employee.salary = employee.salary;
-        if (this.empService.imageURL !='') {
-        this.employee.image = this.empService.imageURL;
-        }else{
+        if (this.empService.imageURL != '') {
+          this.employee.image = this.empService.imageURL;
+        } else {
           this.employee.image = this.oldImage;
         }
 
@@ -71,11 +82,32 @@ export class AddEmployeeComponent implements OnInit {
 
   onsubmit({ value, valid }: { value: Employee, valid: boolean }) {
     if (valid && !this.id) {
-      if (this.empService.imageURL !='') { value.image = this.empService.imageURL; }
-      this.empService.addEmployee(value);
-      this.toastr.success('Employee Added Successfully!', 'Employees', { timeOut: 3000 });
-      this.employee = {} as Employee;
-      this._Router.navigate(['/'])
+
+      const id = Math.random().toString(36).substring(2);
+      this.ref = this.afStorage.ref(id);
+      this.task = this.ref.put(this.myEvent.target.files[0]);
+      this.task.snapshotChanges().pipe(
+        finalize(() => {
+          this.downloadURL = this.ref.getDownloadURL()
+          this.downloadURL.subscribe(url => {
+            value.image = url;
+            this.empService.addEmployee(value);
+            this.employee = {} as Employee;
+            this.toastr.success('Employee Added Successfully!', 'Employees', { timeOut: 3000 });
+            this._Router.navigate(['/']);
+
+          });
+        })
+      )
+        .subscribe();
+
+      
+
+
+
+
+
+
 
     }
   }
@@ -84,13 +116,13 @@ export class AddEmployeeComponent implements OnInit {
     this.employee.image = this.empService.imageURL;
 
     this.empService.updateEmployee(this.employee);
-    
-      setTimeout(() => {
-        this.empService.deleteImage(this.oldImage);
-        console.log('Toooooooot')
-      }, 2000);
 
-   
+    setTimeout(() => {
+      this.empService.deleteImage(this.oldImage);
+      console.log('Toooooooot')
+    }, 2000);
+
+
 
 
     this.toastr.success('Employee Updated Successfully!!', 'Employees', { timeOut: 3000 })
@@ -104,13 +136,14 @@ export class AddEmployeeComponent implements OnInit {
     this.empService.deleteImage(this.oldImage)
   }
 
-  onUpload(event) {
-    this.empService.upload(event);
-  }
+
   ///display image of input file
   onSelectFile(event) { // called each time file input changes
+
     if (event.target.files && event.target.files[0]) {
       var reader = new FileReader();
+      // if (!localStorage.getItem('myFile') || localStorage.getItem('myFile') == '')
+
 
       reader.readAsDataURL(event.target.files[0]); // read file as data url
 
@@ -121,6 +154,8 @@ export class AddEmployeeComponent implements OnInit {
         }
         else {
           this.url = reader.result as string;
+          this.myEvent = event;
+
         }
 
       }
--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 //Angular Fire store  Uploading Files
 import { AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask } from 'angularfire2/storage';
 import { Employee } from "../interfaces/Employee";
-import { finalize } from 'rxjs/operators';
+
 import * as firebase from 'firebase';
 
 
@@ -60,23 +60,7 @@ export class EmployeeService {
   }
 
 
-  upload(event) {
-    const id = Math.random().toString(36).substring(2);
-    this.ref = this.afStorage.ref(id);
-    this.task = this.ref.put(event.target.files[0]);
-    this.task.snapshotChanges().pipe(
-      finalize(() => {
-        this.downloadURL = this.ref.getDownloadURL()
-        this.downloadURL.subscribe(url => {
-          this.imageURL='';
-          this.imageURL = url;
-        });
-               
-      })
-    )
-      .subscribe();
-  }
-
+  
   deleteImage(name: string) {
     const storageRef = firebase.storage().ref();
     storageRef.storage.refFromURL(name).delete();
